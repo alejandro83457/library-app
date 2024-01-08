180a0a8591509e88a34d366108c3a1142c20f410
alejandro83457 <martinezalejandro2001@hotmail.com>
2024-01-07 21:48:52 -0800

    Add read toggle functionality for listed books

--- script.js
+++ script.js
@@ -65,9 +65,31 @@ const addBook = () => {
   let newBook = new Book(book, author, pages, read);
   myLibrary.push(newBook);
 
-  // create div
+  // create div container
   let div = document.createElement("div");
   div.setAttribute("data-value", newBook.name);
+
+  // create div for book attributes
+  let nameDiv = document.createElement("div");
+  let authorDiv = document.createElement("div");
+  let pagesDiv = document.createElement("div");
+  let readDiv = document.createElement("div");
+
+  // create checkbox for read
+  let readCheck = document.createElement("input");
+  readCheck.setAttribute("type", "checkbox");
+  readCheck.checked = read;
+  readCheck.addEventListener("change", (e) => {
+    updateRead(
+      readCheck.parentElement.parentElement.getAttribute("data-value")
+    );
+    console.log(myLibrary);
+  });
+
+  nameDiv.textContent = newBook.name;
+  authorDiv.textContent = newBook.author;
+  pagesDiv.textContent = newBook.pages;
+  readDiv.textContent = "Read ";
 
   // create button
   let button = document.createElement("button");
@@ -83,15 +105,26 @@ const addBook = () => {
     console.log(myLibrary);
   });
 
-  div.textContent = newBook.name;
+  div.appendChild(nameDiv);
+  div.appendChild(authorDiv);
+  div.appendChild(pagesDiv);
+  readDiv.appendChild(readCheck);
+  div.appendChild(readDiv);
   div.appendChild(button);
   bookDiv.appendChild(div);
 };
 
+// removes a book from book array using splice
 const deleteBook = (bookToRemove) => {
   let bookIndex;
   for (let i = 0; i < myLibrary.length; i++) {
     if (myLibrary[i].name == bookToRemove) bookIndex = i;
   }
   myLibrary.splice(bookIndex, 1);
+};
+
+const updateRead = (book) => {
+  for (let curr of myLibrary) {
+    if (curr.name == book) curr.read = !curr.read;
+  }
 };
