1a0b5af51e2420e1089d1208fc0e3c488902e693
alejandro83457 <martinezalejandro2001@hotmail.com>
2024-01-11 22:54:52 -0800

    Successful add/delete book test on library class

---
+++ script-temp.js
@@ -0,0 +1,146 @@
+let addBookButton = document.querySelector("#add-book-button");
+let dialog = document.querySelector("dialog");
+let closeDialogButton = document.querySelector("dialog button");
+let bookDiv = document.querySelector("#book-list");
+
+let bookElement = document.querySelector("dialog #book-name");
+let authorElement = document.querySelector("dialog #author-name");
+let pagesElement = document.querySelector("dialog #pages");
+let readElement = document.querySelector("dialog #read");
+
+let book = "";
+let author = "";
+let pages = 0;
+let read = false;
+
+const myLibrary = [];
+
+// Book object used for storage
+function Book(name, author, pages, read) {
+  this.name = name;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+// Toggle function used to toggle read var.
+// Book.prototype.toggleRead = function () {
+//   this.read = !this.read;
+// };
+
+// Event listeners
+addBookButton.addEventListener("click", () => {
+  dialog.showModal();
+});
+bookElement.addEventListener("input", (e) => {
+  book = e.target.value;
+});
+authorElement.addEventListener("input", (e) => {
+  author = e.target.value;
+});
+pagesElement.addEventListener("input", (e) => {
+  pages = e.target.value;
+});
+readElement.addEventListener("change", (e) => {
+  read = e.target.checked;
+});
+
+// Close dialog if clicking background
+dialog.addEventListener("click", (e) => {
+  if (e.target === dialog) {
+    // reset everything
+    bookElement.value = "";
+    authorElement.value = "";
+    pagesElement.value = "";
+    readElement.checked = false;
+    read = false;
+
+    // close dialog box
+    dialog.close();
+  }
+});
+
+// Closes dialog
+closeDialogButton.addEventListener("click", (e) => {
+  e.preventDefault(); // prevents the submission of the form
+  dialog.close();
+
+  addBook();
+  console.log(myLibrary);
+
+  // Clears previous inputs
+  bookElement.value = "";
+  authorElement.value = "";
+  pagesElement.value = "";
+  readElement.checked = false;
+  read = false;
+});
+
+const addBook = () => {
+  let newBook = new Book(book, author, pages, read);
+  myLibrary.push(newBook);
+
+  // create div container
+  let div = document.createElement("div");
+  div.setAttribute("data-value", newBook.name);
+
+  // create div for book attributes
+  let nameDiv = document.createElement("div");
+  let authorDiv = document.createElement("div");
+  let pagesDiv = document.createElement("div");
+  let readDiv = document.createElement("div");
+
+  // create checkbox for read
+  let readCheck = document.createElement("input");
+  readCheck.setAttribute("type", "checkbox");
+  readCheck.checked = read;
+  readCheck.addEventListener("change", (e) => {
+    updateRead(
+      readCheck.parentElement.parentElement.getAttribute("data-value")
+    );
+    console.log(myLibrary);
+  });
+
+  nameDiv.textContent = newBook.name;
+  authorDiv.textContent = newBook.author;
+  pagesDiv.textContent = `Pages ${newBook.pages}`;
+  readDiv.textContent = "Read ";
+
+  // create button
+  let button = document.createElement("button");
+  button.textContent = "Delete";
+
+  // button event to delete itself and parent
+  button.addEventListener("click", () => {
+    // delete book from array
+    deleteBook(button.parentElement.getAttribute("data-value"));
+
+    // delete book from DOM
+    button.parentElement.remove();
+    console.log(myLibrary);
+  });
+
+  div.appendChild(nameDiv);
+  div.appendChild(authorDiv);
+  div.appendChild(pagesDiv);
+  readDiv.appendChild(readCheck);
+  div.appendChild(readDiv);
+  div.appendChild(button);
+  bookDiv.appendChild(div);
+};
+
+// removes a book from book array using splice
+const deleteBook = (bookToRemove) => {
+  let bookIndex;
+  for (let i = 0; i < myLibrary.length; i++) {
+    if (myLibrary[i].name == bookToRemove) bookIndex = i;
+  }
+  myLibrary.splice(bookIndex, 1);
+};
+
+// updates read property after book added to DOM
+const updateRead = (book) => {
+  for (let curr of myLibrary) {
+    if (curr.name == book) curr.read = !curr.read;
+  }
+};
--- script.js
+++ script.js
@@ -8,139 +8,64 @@ let authorElement = document.querySelector("dialog #author-name");
 let pagesElement = document.querySelector("dialog #pages");
 let readElement = document.querySelector("dialog #read");
 
-let book = "";
-let author = "";
-let pages = 0;
-let read = false;
-
-const myLibrary = [];
-
-// Book object used for storage
-function Book(name, author, pages, read) {
-  this.name = name;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+class Library {
+  #library = [];
+  addBook(book) {
+    this.#library.push(book);
+  }
+  removeBook(book) {
+    // console.log(`Book to remove: ${book.name}`);
+    let index;
+    for (let i = 0; i < this.#library.length; i++) {
+      if (this.#library[i].name == book.name) index = i;
+    }
+    this.#library.splice(index, 1);
+  }
+  showLibrary() {
+    for (let book of this.#library) {
+      console.log(book.book());
+    }
+    console.log(" ");
+  }
 }
 
-// Toggle function used to toggle read var.
-// Book.prototype.toggleRead = function () {
-//   this.read = !this.read;
-// };
-
-// Event listeners
-addBookButton.addEventListener("click", () => {
-  dialog.showModal();
-});
-bookElement.addEventListener("input", (e) => {
-  book = e.target.value;
-});
-authorElement.addEventListener("input", (e) => {
-  author = e.target.value;
-});
-pagesElement.addEventListener("input", (e) => {
-  pages = e.target.value;
-});
-readElement.addEventListener("change", (e) => {
-  read = e.target.checked;
-});
-
-// Close dialog if clicking background
-dialog.addEventListener("click", (e) => {
-  if (e.target === dialog) {
-    // reset everything
-    bookElement.value = "";
-    authorElement.value = "";
-    pagesElement.value = "";
-    readElement.checked = false;
-    read = false;
-
-    // close dialog box
-    dialog.close();
+class Book {
+  #name;
+  #author;
+  #pages;
+  #read;
+  constructor(name, author, pages, read) {
+    this.#name = name;
+    this.#author = author;
+    this.#pages = pages;
+    this.#read = read;
   }
-});
-
-// Closes dialog
-closeDialogButton.addEventListener("click", (e) => {
-  e.preventDefault(); // prevents the submission of the form
-  dialog.close();
-
-  addBook();
-  console.log(myLibrary);
-
-  // Clears previous inputs
-  bookElement.value = "";
-  authorElement.value = "";
-  pagesElement.value = "";
-  readElement.checked = false;
-  read = false;
-});
-
-const addBook = () => {
-  let newBook = new Book(book, author, pages, read);
-  myLibrary.push(newBook);
-
-  // create div container
-  let div = document.createElement("div");
-  div.setAttribute("data-value", newBook.name);
-
-  // create div for book attributes
-  let nameDiv = document.createElement("div");
-  let authorDiv = document.createElement("div");
-  let pagesDiv = document.createElement("div");
-  let readDiv = document.createElement("div");
-
-  // create checkbox for read
-  let readCheck = document.createElement("input");
-  readCheck.setAttribute("type", "checkbox");
-  readCheck.checked = read;
-  readCheck.addEventListener("change", (e) => {
-    updateRead(
-      readCheck.parentElement.parentElement.getAttribute("data-value")
-    );
-    console.log(myLibrary);
-  });
-
-  nameDiv.textContent = newBook.name;
-  authorDiv.textContent = newBook.author;
-  pagesDiv.textContent = `Pages ${newBook.pages}`;
-  readDiv.textContent = "Read ";
-
-  // create button
-  let button = document.createElement("button");
-  button.textContent = "Delete";
-
-  // button event to delete itself and parent
-  button.addEventListener("click", () => {
-    // delete book from array
-    deleteBook(button.parentElement.getAttribute("data-value"));
-
-    // delete book from DOM
-    button.parentElement.remove();
-    console.log(myLibrary);
-  });
-
-  div.appendChild(nameDiv);
-  div.appendChild(authorDiv);
-  div.appendChild(pagesDiv);
-  readDiv.appendChild(readCheck);
-  div.appendChild(readDiv);
-  div.appendChild(button);
-  bookDiv.appendChild(div);
-};
-
-// removes a book from book array using splice
-const deleteBook = (bookToRemove) => {
-  let bookIndex;
-  for (let i = 0; i < myLibrary.length; i++) {
-    if (myLibrary[i].name == bookToRemove) bookIndex = i;
+  toggleRead() {
+    this.#read = !this.#read;
   }
-  myLibrary.splice(bookIndex, 1);
-};
-
-// updates read property after book added to DOM
-const updateRead = (book) => {
-  for (let curr of myLibrary) {
-    if (curr.name == book) curr.read = !curr.read;
+  get name() {
+    return this.#name;
   }
-};
+  get author() {
+    return this.#author;
+  }
+  get pages() {
+    return this.#pages;
+  }
+  book() {
+    return `${this.#name} ${this.#author}`;
+  }
+}
+
+let book1 = new Book("name1", "author1", 100, false);
+let book2 = new Book("name2", "author2", 100, true);
+let book3 = new Book("name3", "author3", 100, false);
+
+let library = new Library();
+library.addBook(book1);
+library.addBook(book2);
+library.addBook(book3);
+library.showLibrary();
+
+library.removeBook(book3);
+library.showLibrary();
