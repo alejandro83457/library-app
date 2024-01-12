78b2a3b52aec1ca127f7d55549532b3a8d5852ef
alejandro83457 <martinezalejandro2001@hotmail.com>
2024-01-11 23:13:19 -0800

    Implement book addition to library using dialog

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-let addBookButton = document.querySelector("#add-book-button");
-let dialog = document.querySelector("dialog");
-let closeDialogButton = document.querySelector("dialog button");
-let bookDiv = document.querySelector("#book-list");
-
-let bookElement = document.querySelector("dialog #book-name");
-let authorElement = document.querySelector("dialog #author-name");
-let pagesElement = document.querySelector("dialog #pages");
-let readElement = document.querySelector("dialog #read");
-
 class Library {
   #library = [];
   addBook(book) {
@@ -53,19 +43,69 @@ class Book {
     return this.#pages;
   }
   book() {
-    return `${this.#name} ${this.#author}`;
+    return `${this.#name} ${this.#author} ${this.#pages} ${this.#read}`;
   }
 }
 
-let book1 = new Book("name1", "author1", 100, false);
-let book2 = new Book("name2", "author2", 100, true);
-let book3 = new Book("name3", "author3", 100, false);
+// Element objects
+let addBookButton = document.querySelector("#add-book-button");
+let dialog = document.querySelector("dialog");
+let closeDialogButton = document.querySelector("dialog button");
+let bookDiv = document.querySelector("#book-list");
 
-let library = new Library();
-library.addBook(book1);
-library.addBook(book2);
-library.addBook(book3);
-library.showLibrary();
+let bookElement = document.querySelector("dialog #book-name");
+let authorElement = document.querySelector("dialog #author-name");
+let pagesElement = document.querySelector("dialog #pages");
+let readElement = document.querySelector("dialog #read");
 
-library.removeBook(book3);
-library.showLibrary();
+// Library object
+const library = new Library();
+
+// EVENT LISTENERS
+addBookButton.addEventListener("click", () => {
+  dialog.showModal();
+});
+// Close dialog if clicking background
+dialog.addEventListener("click", (e) => {
+  if (e.target === dialog) {
+    bookElement.value = "";
+    authorElement.value = "";
+    pagesElement.value = "";
+    readElement.checked = false;
+    // close dialog box
+    dialog.close();
+  }
+});
+// Closes dialog
+closeDialogButton.addEventListener("click", (e) => {
+  e.preventDefault(); // prevents the submission of the form
+  dialog.close();
+
+  let name = bookElement.value;
+  let author = authorElement.value;
+  let pages = pagesElement.value;
+  let read = readElement.checked;
+
+  let book = new Book(name, author, pages, read);
+  library.addBook(book);
+  library.showLibrary();
+
+  // Clears previous inputs
+  bookElement.value = "";
+  authorElement.value = "";
+  pagesElement.value = "";
+  readElement.checked = false;
+});
+
+// let book1 = new Book("name1", "author1", 100, false);
+// let book2 = new Book("name2", "author2", 100, true);
+// let book3 = new Book("name3", "author3", 100, false);
+
+// let library = new Library();
+// library.addBook(book1);
+// library.addBook(book2);
+// library.addBook(book3);
+// library.showLibrary();
+
+// library.removeBook(book3);
+// library.showLibrary();
